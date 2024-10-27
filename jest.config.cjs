a48ef0234c452415850b5b7edef549cbdfdd017f
Aimen <aimen.el-sayed@stud.fh-campuswien.ac.at>
2024-10-27 15:50:52 +0100

    remove preinstall

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,11 +1,10 @@
 module.exports = {
     preset: 'ts-jest',
+    testEnvironment: 'jsdom',
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
     transform: {
         '^.+\\.tsx?$': 'ts-jest',
-        '^.+\\.jsx?$': 'babel-jest', // Add this line to handle ES modules
     },
-    testEnvironment: 'jsdom',
     moduleNameMapper: {
         '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
     },
